5a6fd872f937f3994e3764dfadde549bfe38d078
0xNeon <anonymous_crypto_person@protonmail.com>
2021-11-02 17:07:00 -0400

    trying to reduce build size

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HelloArweave from './components/HelloArweave';
 import Web3Connection from './web3/Web3Connection';
 
 function App() {
   return (
     <div className="App">
       <Web3Connection />
-      <HelloArweave />
     </div>
   );
 }
