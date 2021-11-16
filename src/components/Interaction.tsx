dae51c814f96f1f3503537ab7b51fc1f2505dd13
0xNeon <anonymous_crypto_person@protonmail.com>
2021-11-15 22:31:00 -0500

    cleaned up code

--- src/components/Interaction.tsx
+++ src/components/Interaction.tsx
@@ -2,25 +2,12 @@ import * as React from 'react';
 import Fade from '@mui/material/Fade';
 import { initialGreeting } from './speech';
 
-function fadeIn(speech: string) {
-    return (
-        <Fade in={true} timeout={1000}>
-            <span>{speech}</span>
-        </Fade>
-    )
-}
-
-
-interface InteractionProps {
-
-}
-
 interface InteractionState {
     currentSpeech: string,
     indexToFade: number
 }
 
-class Interaction extends React.Component<InteractionProps, InteractionState> {
+class Interaction extends React.Component<{}, InteractionState> {
     state = {
         currentSpeech: '',
         indexToFade: -1
@@ -29,22 +16,24 @@ class Interaction extends React.Component<InteractionProps, InteractionState> {
     async componentDidMount() {
         await this.sleep(3000);
         this.setState({ currentSpeech: initialGreeting });
-        for (let index = 0; index < this.state.currentSpeech.length; index++) {
-            await this.sleep(100);
-            this.setState({ indexToFade: index });
-            
-        }
+        await this.fadeInCurrentSpeech();
     }
 
     createFadeIn(speech: string): JSX.Element[] {
-        const speechArray = speech.split(' ');
+        const speechArray = speech.split('');
         const ret: JSX.Element[] = []
         for (let i = 0; i < speechArray.length; i++) {
-            const n = <Fade in={this.state.indexToFade >= i}><span>{speechArray[i] + ' '}</span></Fade>
+            const n = <Fade in={this.state.indexToFade >= i} key={i}><span>{speechArray[i]}</span></Fade>
             ret.push(n);
         }
-
         return ret;
+    }
+
+    async fadeInCurrentSpeech() {
+        for (let index = 0; index < this.state.currentSpeech.length; index++) {
+            await this.sleep(50);
+            this.setState({ indexToFade: index });
+        }
     }
 
     async sleep(ms: number) {
--- src/components/Matsuo/MatsuoRenderer.js
+++ src/components/Matsuo/MatsuoRenderer.js
@@ -15,15 +15,15 @@ export default class Canvas extends React.Component {
             // https://barradeau.com/blog/?p=621
             // This examples needs WebGL1 (?)
             gl: new THREE.WebGL1Renderer({
-              canvas: document.querySelector('canvas'),
-              antialias: true,
-              alpha: true
+                canvas: document.querySelector('canvas'),
+                antialias: true,
+                alpha: true
             })
-          })
+        })
     }
     render() {
         return (
-            <canvas ref="canvas" width={'35vh'} height={'35vh'}/>
+            <canvas style={{ width: '35vh', height: '35vh' }} />
         );
     }
 }
