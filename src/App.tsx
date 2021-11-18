c13a12129887bf909094807ae139937823defecd
the0xcollective <90662438+0xNeon-opensource@users.noreply.github.com>
2021-11-17 21:09:38 -0600

    created Terms and Conditions ⚖

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import TermsAndConditions from './components/TermsAndConditions';
 
 const Web3Connection = lazy(() => import('./web3/Web3Connection'));
 
@@ -14,6 +15,8 @@ function App() {
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route path='/mint' component={Web3Connection}></Route>
+            <Route path='/terms' component={TermsAndConditions}></Route>
+            <Route path='/paper' component={Web3Connection}></Route>
             <Route path='/' component={Home}></Route>
           </Switch>
         </Suspense>
