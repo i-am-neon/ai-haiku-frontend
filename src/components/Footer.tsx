7b40a7a1a3defd26de82894b6cee60dee97793ef
the0xcollective <90662438+0xNeon-opensource@users.noreply.github.com>
2021-11-30 17:18:57 -0600

    Added OpenSea Link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,11 @@ export default function Footer() {
         <div style={{ textAlign: 'left', position: 'absolute', bottom: '0.25rem', width: '100%', height: '2rem' }}>
             <a href='/terms' style={{ paddingLeft: '1rem', paddingRight: '1rem'}}>Terms</a>
             <a href='https://etherscan.io/token/0x60129872C5e9decAfa95f77fA6983182aA1a5E88' target="_blank" rel="noreferrer" style={{ paddingLeft: '1rem', paddingRight: '1rem'}}>Contract</a>
+            <a href='https://opensea.io/collection/ai-haiku' target="_blank" rel="noreferrer" style={{ paddingLeft: '1rem', paddingRight: '1rem'}}>OpenSea</a>
             <IconButton href='https://twitter.com/aihaikuNFTs' target='_blank' aria-label="Twitter" color={'inherit'}>
                 <TwitterIcon fill="black" style={{ height: '4vh', maxHeight: '20px' }} />
             </IconButton>
-            <IconButton href="https://discord.gg/aihaiku" target='_blank' aria-label="Discord" color={'inherit'}>
+            <IconButton href="https://discord.gg/aihaiku" target='_blank' rel="noreferrer" aria-label="Discord" color={'inherit'}>
                 <DiscordIcon fill="black" style={{ height: '4vh', maxHeight: '20px' }} />
             </IconButton>
         </div>
